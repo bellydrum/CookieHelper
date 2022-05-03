e5069492f37c3403be18543b1d01e5cd3a7ad8c5
David Maness <9682088+bellydrum@users.noreply.github.com>
2022-05-03 00:20:06 -0500

    add expiration param to add()

--- CookieHelper.js
+++ CookieHelper.js
@@ -7,67 +7,72 @@
 
 class CookieHelper {
 
-    show(cookieObject={}) {
-        if (cookieObject.length !== undefined) {
-            console.log('show() does not take any arguments.')
-            return null
-        }
-        document.cookie.split('; ').forEach(item => {
-            cookieObject[item.split('=')[0]] = item.split('=')[1]
-        })
-        return cookieObject
+  show(cookieObject={}) {
+    if (cookieObject.length !== undefined) {
+      console.log('show() does not take any arguments.')
+      return null
     }
+    document.cookie.split('; ').forEach(item => {
+      cookieObject[item.split('=')[0]] = item.split('=')[1]
+    })
+    return cookieObject
+  }
 
-    hasKey(key) {
-        if (typeof key !== 'string' || key === undefined) {
-            console.log('has() takes a single string argument.')
-            return null
-        }
-        const cookieObject = this.show()
-        return Object.keys(cookieObject).includes(key)
+  hasKey(key) {
+    if (typeof key !== 'string') {
+      console.log('has() takes a single string argument.')
+      return null
     }
+    const cookieObject = this.show()
+    return Object.keys(cookieObject).includes(key)
+  }
 
-    add(object) {
-        if (object === undefined || typeof(object) !== 'object' || Array.isArray(object)) {
-            console.log('add() takes a non-empty object argument.')
-            return null
-        }
-        Object.keys(object).forEach(key => {
-            document.cookie = `${key}=${object[key]};`
-        })
+  add(object, expirationHours=6) {
+    if (object === undefined || typeof(object) !== 'object' || Array.isArray(object)) {
+      console.log('add() takes a non-empty object argument.')
+      return null
     }
+    const date = new Date();
+    date.setTime(date.getTime() + (3600 * expirationHours));
+    const expires = date.toUTCString();
+    let cookieString = '';
+    Object.keys(object).forEach(key => {
+      cookieString += `${key}=${object[key]};`
+    })
+    cookieString += `expires=${expires};path=/`
+    document.cookie = cookieString;
+  }
 
-    getObjectByKey(key, returnObject={}) {
-        if (typeof key !== 'string') {
-            console.log('getObject() takes a single string argument.')
-            return null
-        }
-        returnObject[key] = this.getValueByKey(key)
-        return returnObject
+  getObjectByKey(key, returnObject={}) {
+    if (typeof key !== 'string') {
+      console.log('getObject() takes a single string argument.')
+      return null
     }
+    returnObject[key] = this.getValueByKey(key)
+    return returnObject
+  }
 
-    getValueByKey(key) {
-        if (typeof key !== 'string') {
-            console.log('getValue() takes a single string argument.')
-            return null
-        }
-        const cookieObject = this.show()
-        return cookieObject[key]
+  getValueByKey(key) {
+    if (typeof key !== 'string') {
+      console.log('getValue() takes a single string argument.')
+      return null
     }
+    const cookieObject = this.show()
+    return cookieObject[key]
+  }
 
-    deleteByKey(key) {
-        if (typeof key !== 'string') {
-            console.log('delete() takes a single string argument.')
-            return null
-        }
-        document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+  deleteByKey(key) {
+    if (typeof key !== 'string') {
+      console.log('delete() takes a single string argument.')
+      return null
     }
+    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+  }
 
-    flush(keys=Object.keys(this.show())) {
-        keys.forEach(key => {
-            this.deleteByKey(key)
-        })
-        return this.show()
-    }
-
+  flush(keys=Object.keys(this.show())) {
+    keys.forEach(key => {
+      this.deleteByKey(key)
+    })
+    return this.show()
+  }
 }
