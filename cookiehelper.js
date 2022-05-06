677b332085e7d537aa669c02b0dd05574141211e
David Maness <9682088+bellydrum@users.noreply.github.com>
2022-05-06 12:52:02 -0500

    export cookie object; update filename

--- cookiehelper.js
+++ cookiehelper.js
@@ -1,32 +1,32 @@
 /**
-  * cookiehelper.js
-  * ------------------------------------------------------------------------------
-  * allows handling of cookies in an object-oriented manner.
-  * written by bellydrum
-  */
+ * cookie.es6
+ * ------------------------------------------------------------------------------
+ * allows handling of cookies in an object-oriented manner.
+ * written by bellydrum
+ */
 
-class CookieHelper {
+class Cookie {
   /**
-    * Creates and returns a tool for accessing and updating the document cookie.
-    *
-    * @see     Document.cookie
-    * @link    https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
-    *
-    * @see     CookieHelper
-    * @link    https://github.com/bellydrum/CookieHelper/edit/master/CookieHelper.js
-    *
-    * @returns {Object} CookieHelper
-    *   A Javascript object with methods that allow access to the document cookie.
-    *
-    */
+   * Creates and returns a tool for accessing and updating the document cookie.
+   *
+   * @see     Document.cookie
+   * @link    https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+   *
+   * @see     Cookie
+   * @link    https://github.com/bellydrum/CookieHelper/edit/master/CookieHelper.js
+   *
+   * @returns {Object} Cookie
+   *   A Javascript object with methods that allow access to the document cookie.
+   *
+   */
 
   show(cookieObject={}) {
     /**
-      * Returns the document cookie as a Javascript object.
-      *
-      * @returns {Object<name, value>} cookieObject
-      *   Object whose entries are the keys and values of the document cookie at the time of execution.
-      */
+     * Returns the document cookie as a Javascript object.
+     *
+     * @returns {Object<name, value>} cookieObject
+     *   Object whose entries are the keys and values of the document cookie at the time of execution.
+     */
     if (cookieObject.length !== undefined) {
       console.log('show() does not take any arguments.')
       return null
@@ -39,11 +39,11 @@ class CookieHelper {
 
   hasKey(key) {
     /**
-      * Determines the presence of a cookie by its provided name.
-      *
-      * @param {string} key
-      * @returns {bool}, null
-      */
+     * Determines the presence of a cookie by its provided name.
+     *
+     * @param {string} key
+     * @returns {bool}, null
+     */
     if (typeof key !== 'string') {
       console.log('has() takes a single string argument.')
       return null
@@ -54,17 +54,17 @@ class CookieHelper {
 
   add(object, expirationSeconds=21600) {
     /**
-      * Updates the document cookie.
-      *
-      * @note
-      *   Immediately alters the document cookie with all given {key : value} pairs.
-      *
-      * @param {Object<name, value>} object
-      *   Name and value pairs to be added to the cookie.
-      * @param {number} expirationSeconds
-      *   Number of seconds until the cookie(s) expire.
-      * @returns {null}
-      */
+     * Updates the document cookie.
+     *
+     * @note
+     *   Immediately alters the document cookie with all given {key : value} pairs.
+     *
+     * @param {Object<name, value>} object
+     *   Name and value pairs to be added to the cookie.
+     * @param {number} expirationSeconds
+     *   Number of seconds until the cookie(s) expire.
+     * @returns {null}
+     */
     if (object === undefined || typeof(object) !== 'object' || Array.isArray(object)) {
       console.log('add() takes a non-empty object argument.')
       return null
@@ -82,12 +82,12 @@ class CookieHelper {
 
   getObjectByKey(name) {
     /**
-      * Takes a name and returns it with its value according to the document cookie.
-      *
-      * @param {string} name
-      *   Used to parse the document cookie for a value.
-      * @returns {Object<name, value>} returnObject
-      */
+     * Takes a name and returns it with its value according to the document cookie.
+     *
+     * @param {string} name
+     *   Used to parse the document cookie for a value.
+     * @returns {Object<name, value>} returnObject
+     */
     const returnObject = {}
     if (typeof name !== 'string') {
       console.log('getObject() takes a single string argument.')
@@ -99,13 +99,13 @@ class CookieHelper {
 
   getValueByKey(name) {
     /**
-      * Takes a name and returns only its value according to the document cookie.
-      *
-      * @param {string} name
-      *  Used to parse the document cookie for a value.
-      * @returns {string}
-      *  The value of the given name according to the document cookie.
-      */
+     * Takes a name and returns only its value according to the document cookie.
+     *
+     * @param {string} name
+     *  Used to parse the document cookie for a value.
+     * @returns {string}
+     *  The value of the given name according to the document cookie.
+     */
     if (typeof name !== 'string') {
       console.log('getValueByKey() takes a single string argument.')
       return null
@@ -116,18 +116,18 @@ class CookieHelper {
 
   deleteByKey(name) {
     /**
-      * Takes a name and deletes its value in the document cookie.
-      *
-      * @note
-      *   Immediately alters the document cookie.
-      *
-      * @see     How to delete a cookie.
-      * @link    https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#Notes
-      *
-      * @param {string} name
-      *   Name of the {name: value} pair to delete from the document cookie.
-      * @returns {null}
-      */
+     * Takes a name and deletes its value in the document cookie.
+     *
+     * @note
+     *   Immediately alters the document cookie.
+     *
+     * @see     How to delete a cookie.
+     * @link    https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#Notes
+     *
+     * @param {string} name
+     *   Name of the {name: value} pair to delete from the document cookie.
+     * @returns {null}
+     */
     if (typeof name !== 'string') {
       console.log('deleteByKey() takes a single string argument.')
       return null
@@ -137,16 +137,16 @@ class CookieHelper {
 
   flush(names=Object.keys(this.show())) {
     /**
-      * Empties the document cookie.
-      *
-      * @note
-      *   Immediately alters the document cookie.
-      *
-      * @see this.deleteByKey()
-      *
-      * @returns {Object<name, value>}
-      *   Object that contains only the values of the document cookie unable to be deleted.
-      */
+     * Empties the document cookie.
+     *
+     * @note
+     *   Immediately alters the document cookie.
+     *
+     * @see this.deleteByKey()
+     *
+     * @returns {Object<name, value>}
+     *   Object that contains only the values of the document cookie unable to be deleted.
+     */
     names.forEach(name => {
       this.deleteByKey(name)
     })
@@ -154,4 +154,4 @@ class CookieHelper {
   }
 }
 
-const cookie = new CookieHelper();
+export const cookie = new Cookie();
